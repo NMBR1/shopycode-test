89aa1b225d5f5cd448a12dd6d7c2cd6abd5657bf
= <=>
2021-04-14 13:54:19 +0200

    latest changes

--- skripta.js
+++ skripta.js
@@ -26,19 +26,18 @@ const querystring = require('querystring');
 const clientID = '446975523244761';
 const instagramAppSecret = 'a2931c610cc0b8bc65fcfcc97b1189ad';
 const redirecUrl = 'https://shopycode-test.herokuapp.com/';
-const code = 'AQAx8re4KPQEOjXjFglhtbgvqbAzc08P8RFEuIoq2UbOOemcNEhZcWf7LF6VrXpi6ErvRBKLKotFAUzKO-KtpjqRZP-48LyxtKTk8sXZf_FDqINgrJFsEMaYSbSfTCYide39PrPc_e_XcQAXWJsJvBDNH7xK84y3zBsumLzvEiofUZezLmNTShM-v1KhVJX1tPFQNPbpuaIDGfuHUZk18TOG05kR9IlScONX5bpKHYcJyg#_';
+const code = 'AQAs9AA58wrzT4GqQojV4eDF6hGhpyPPGs-gFxxaMEOV2ylWb7AaxkyEzvbsaJsaFbikdp9Q7F_39_balUOi3Yrdn3uVz3lzdLwnVDW-Hj7y6r_LFGeeLM-YFyz9vkFUlu32CkFrJ70ZewVDKqkudYqe4ZFzNhGCqo9kUr9fB_tG69AYf9JmUpBI-Gg4AEpsy2GP4meLxcSX-utyysidP79IlB5aOG2y1Jy6rQavbx7FEQ';
 
 
 async function asyncCall() {
- var res = await axios.post('https://shopycode-test.herokuapp.com/', { }, {
-    headers: {
+ var res = await axios.post('https://shopycode-test.herokuapp.com/',  {
         client_id:`${clientID}`,
         client_secret:`${instagramAppSecret}`,
         grant_type:"authorization_code",
         redirect_uri:`${redirecUrl}`,
         code:`${code}`
       }
-    }).catch((err) => {
+    ).catch((err) => {
         console.log(err);
     });
     console.log(res);
--- testing.js
+++ testing.js
@@ -1,27 +1,27 @@
-const Instagram = require('instagram-web-api');
-const { username, password } = process.env;
+// const Instagram = require('instagram-web-api');
+// const { username, password } = process.env;
 
-const client = new Instagram({ username: 'shopycode_agency' , password: 'Fudbaleri89!!'});
+// const client = new Instagram({ username: 'shopycode_agency' , password: 'Fudbaleri89!!'});
 
-// const feed = await client.getHome('IGQVJVekxFVmZAOVUpIamhLRjIzcnc0TWJGeWlHNnV5MVFQTlhqR3ZADQndYMFJlcFplWjVnd3dvek4zc0JOZAElPZAmRhNmJFbk9VMEg0QWpRNndZAWmk4WEdycE5ac1MxQ3ZAtemtHUGd2aWllN3Itc1pFejhrTGdKeGFaV2pr');
+// // const feed = await client.getHome('IGQVJVekxFVmZAOVUpIamhLRjIzcnc0TWJGeWlHNnV5MVFQTlhqR3ZADQndYMFJlcFplWjVnd3dvek4zc0JOZAElPZAmRhNmJFbk9VMEg0QWpRNndZAWmk4WEdycE5ac1MxQ3ZAtemtHUGd2aWllN3Itc1pFejhrTGdKeGFaV2pr');
 
-(async () => {
-    await client.login()
-    const profile = await client.getProfile()
-    const feed = await client.getHome();
-    const photo = 'https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg';
-    //const { media } = await client.uploadPhoto({ photo: photo, caption: 'Development', post: 'feed' })
-    const photos = await client.getPhotosByUsername({ username: 'shopycode_agency' });
-    //console.log(`https://www.instagram.com/p/${media.code}/`);
+// (async () => {
+//     await client.login()
+//     const profile = await client.getProfile()
+//     const feed = await client.getHome();
+//     const photo = 'https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg';
+//     //const { media } = await client.uploadPhoto({ photo: photo, caption: 'Development', post: 'feed' })
+//     const photos = await client.getPhotosByUsername({ username: 'shopycode_agency' });
+//     //console.log(`https://www.instagram.com/p/${media.code}/`);
 
 
-    console.log(profile);
-    console.log(feed);
-    const slike = photos.user.edge_owner_to_timeline_media;
-    console.log(slike.edges[0].node.thumbnail_resources);
+//     console.log(profile);
+//     console.log(feed);
+//     const slike = photos.user.edge_owner_to_timeline_media;
+//     console.log(slike.edges[0].node.thumbnail_resources);
 
  
 
 
 
-  })()
+//   })()
